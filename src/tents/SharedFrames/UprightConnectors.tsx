e69bc7cf8516b568dd0036d4a21e156ac2f7d298
xGlobal <xglobaltents@gmail.com>
2026-02-27 04:22:01 +0400

    feat: Add initial UprightConnectors component with basic structure

---
+++ src/tents/SharedFrames/UprightConnectors.tsx
@@ -0,0 +1,8 @@
+import type { FC } from 'react'
+import type { FrameComponentProps } from '@/types'
+
+export const UprightConnectors: FC<FrameComponentProps> = () => {
+	return null
+}
+
+export default UprightConnectors
